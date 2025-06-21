ba7d8dd5f87513c1ca7049a16f1b90d236baea10
talon33-dev <talon1818@gmail.com>
2025-06-21 16:56:35 -0700

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}"
+  ],
+  theme: {
+    extend: {
+      colors: {
+        tan: '#D2B48C',
+        offgrey: '#A9A9A9'
+      }
+    }
+  },
+  plugins: [],
+}
